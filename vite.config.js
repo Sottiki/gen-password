e8b516124346187a31a2d143ed2138b422a03337
Sottiki <81673075+Sottiki@users.noreply.github.com>
2025-09-06 00:22:31 +0900

    add testing lib & add sample test

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tsconfigPaths()],
+    test: {
+        environment: 'jsdom', // DOM環境を提供
+        setupFiles: './vitest.setup.js',
+        css: true, // CSS importを許容
+        globals: true, // グローバルにdescribeやtestを使えるようにする
+    },
 });
